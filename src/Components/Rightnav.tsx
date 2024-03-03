6fcb30656fdb01350c1e8d9e3d529e03fd6e4b4a
Shivamzee <hello.shivamzee@gmail.com>
2024-03-03 15:13:12 +0530

    final commit

--- src/Components/Rightnav.tsx
+++ src/Components/Rightnav.tsx
@@ -1,26 +1,25 @@
+type newsProp = {
+  gnews: any;
+};
 
-
-type newsProp ={
-    gnews:any
-}
-
-const Rightnav = (props:newsProp) => {
+const Rightnav = (props: newsProp) => {
   return (
-    <div className='w-60 ml-5'>
-     {props.gnews.map((data:any)=>{
-        return <>
-         <a href={data.url}>
-          <ul className="list-disc marker:text-blue-600 mt-5">
-            <li className="font-bold">{data.title}</li>
-          </ul>
-         </a>
+    <div className="w-60 ml-5">
+      {props.gnews.map((data: any) => {
+        return (
+          <>
+            <a href={data.url}>
+              <ul className="list-disc marker:text-blue-600 mt-5">
+                <li className="font-bold">{data.title}</li>
+              </ul>
+            </a>
 
-             
-             <hr className="mt-5"/>
-        </>
-     })}
+            <hr className="mt-5" />
+          </>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Rightnav
+export default Rightnav;
